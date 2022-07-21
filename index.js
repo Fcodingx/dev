97ff69f2d007d947a33f6ece155805ea5993e14c
Fcodingx <fcodingx@gmail.com>
2022-07-20 21:20:40 -0500

    7

---
+++ index.js
@@ -0,0 +1,8 @@
+let nombre = prompt ('Dame un nombre')
+let apellido = prompt ('Dame un apellido')
+let edad = prompt ('Dame una edad')
+
+let fullname = nombre + ' ' + apellido
+let age =  parseInt(edad)
+
+alert('El nombre es ' + fullname + ' y su edad es de ' + age)
